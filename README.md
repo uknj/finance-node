771fc9891208fea1853b8e43b43fa76828dc661c
Uknj <umar1995_khan@hotmail.com>
2014-07-20 13:32:40 +0300

    updating readme, placing comment blocks in code to readme. Updating finance-node_v2 to place attributes and values in rows, not columns.

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
-  _    _ _  ___   _      _ 
- | |  | | |/ / \ | |    | |
- | |  | | ' /|  \| |    | |
- | |  | |  < | . ` |_   | |
- | |__| | . \| |\  | |__| |
-  \____/|_|\_\_| \_|\____/ 
-                           
-                           
+* * * * * - - - - - * * * * *
+
+
+Finance Node Version 2
+
+Original Author: Ido Green | plus.google.com/+Greenido
+Date: 15 Aug 2013
+
+Fetch data from the web and save it into a Google Sheet document. (Google Id and password)
+
+Requirements:
+  1. NodeJS
+  2. npm install request
+  3. npm install cheerio
+  4. npm install edit-google-spreadsheet
+
+Current Author: Umar "uknj" Khan | twiiter.com/theuknj | facebook.com/the.uknj | github.com/uknj
+Date: 18 Jul 2014
+Further requirements:
+    None as of yet!
+
+
+Objectives:
+	1. Convert from column to row storage. DONE :)
+	2. Allow stock searches from CLI and not from code.
+	3. Save multiple stock searches in the same worksheet.
+	4. Attempting to allow for multiple stock searches. --DELAYED.
--- finance-node_v2.js
+++ finance-node_v2.js
@@ -1,76 +1,59 @@
-/**
-* Fetch data from the web and save it into a Google Sheet document
-*
-* Requirements:
-  1. NodeJS
-  2. npm install request
-  3. npm install cheerio
-  4. npm install edit-google-spreadsheet
-
-* Author: Ido Green | plus.google.com/+Greenido
-* Date: 15 Aug 2013
-*/
-
 var request     = require('request');
 var cheerio     = require('cheerio');
 var Spreadsheet = require('edit-google-spreadsheet');
-
+ 
 // Some parameters 
 // TODO: move from the global scope
-var ticker  = "LVS";
+var ticker  = "AAPL";
 var yUrl    = "http://finance.yahoo.com/q/ks?s=" + ticker;
 var financeDetails = new Array();
 var keyStr         = new Array();
-
+var rowValue       = 2;
 
 // Upload our data to G-Sheet
 function sheetReady(err, spreadsheet) {
     if (err) throw err;
-    spreadsheet.add({ 1: { 1: "Attribute" } });
-    spreadsheet.add({ 1: { 2: "Value" } });
-   
-    spreadsheet.add({ 
-      2: {
-        1: keyStr
-      }
-    });
-    spreadsheet.add({ 
-      2: {
-        2: financeDetails
-      }
-    });
+    // spreadsheet.add({ 1: { 1: "Attribute" } }); // Adds text "Attribute" to A1. Not needed.
+    // spreadsheet.add({ 2: { 1: "Value" } }); // Adds text "Value" to A2. Not needed.
+    spreadsheet.add({ 1: { 1: "Name" } }); // Adds text "Name" to Cell A1.
+    spreadsheet.add({ 2: { 1: ticker } }); // Adds Ticker name to Cell A2.
 
+
+    spreadsheet.add({ 1: { 2:[ keyStr] }  }); // Adds finance attributes to Row 1.
+    spreadsheet.add({ 2: { 2: [ financeDetails ] } }); // Adds values to Row 2.
+    
     spreadsheet.send(function(err) {
       if(err) throw err;
       console.log("Updated " + financeDetails.length + " Items with data");
     });
   }
 
+ 
 //
 // The main call to fetch the data, parse it and work on it.  
 //
 request(yUrl, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(body);
-
+ 
     // the keys - We get them from a certain class attribute
     var td = $('.yfnc_tablehead1');
     $(td).each(function(j, val) {
       keyStr[j] = $(val).text();
     });
-
+ 
     // the values
     // TODO: normalize them 
     var tData = $('.yfnc_tabledata1');
     $(tData).each(function(j, val) {
       financeDetails[j] = $(val).text();
     });
-
+ 
     // Let's do something with the data we have
     for (var i=0; i < financeDetails.length; i++) {
       console.log (i + ") " + keyStr[i] + " " + financeDetails[i]);
     }
-
+ 
     // upload our data to Google sheet
     // yFinanceDataPerComapy
     Spreadsheet.create({
@@ -78,8 +61,8 @@ request(yUrl, function (error, response, body) {
       username: 'TODO-fill',
       password: 'TODO-fill',
       debug: true,
-      spreadsheetName: 'TODO-yourSheetName',
-      worksheetName: 'TODO-Sheet1orAbetterName',
+      spreadsheetId: 'TODO-yourSheetName',
+      worksheetId: 'TODO-Sheet1orAbetterName',
       callback: sheetReady
     });
   }
